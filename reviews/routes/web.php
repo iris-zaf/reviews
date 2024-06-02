971a2451e8a1d20f23fa198916418bee6ffe008c
Iris <iriskalogirou1@gmail.com>
2024-06-02 19:17:35 +0300

    adding basic admmin and auth with breeze

---
+++ reviews/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+//user routes
+Route::middleware(['auth', 'userMiddleware'])->group(function () {
+    Route::get('dashboard', [UserController::class, 'index'])->name('dashboard');
+    Route::get('order', [UserController::class, 'order'])->name('user.order');
+    Route::get('favorite', [UserController::class, 'favorite'])->name('user.favorite');
+    Route::get('profile/edit', [UserController::class, 'profile'])->name('user.profile.edit');
+});
+
+//admin
+Route::middleware(['auth', 'adminMiddleware'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/admin/product', [AdminController::class, 'product'])->name('admin.product');
+    Route::get('/admin/category', [AdminController::class, 'category'])->name('admin.category');
+    Route::get('/admin/user', [AdminController::class, 'user'])->name('admin.user');
+});
